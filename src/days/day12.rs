95a3cccc33bb1975b564a1fe3f790ce0ed88495d
Antoine POPINEAU <antoine@popineau.eu>
2023-12-12 19:29:32 +0100

    Optimizing Day 12.

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,15 +1,16 @@
 use std::iter::repeat;
 
 use itertools::intersperse;
-use rustc_hash::FxHashMap;
 
 use crate::util::{self, parse};
 
 crate::tests!(12, (21, 525152));
 
-type Cache<'a> = FxHashMap<(&'a [char], &'a [i64]), i64>;
+const CACHE_SIZE: usize = 1 << 12;
 
-fn parse_springs(input: &str, folds: usize) -> Vec<(Vec<char>, Vec<i64>)> {
+type Cache<'a> = [i64; CACHE_SIZE];
+
+fn parse_springs(input: &str, folds: usize) -> Vec<(Vec<char>, Vec<u8>)> {
   let lines = util::read_file_lines(input);
 
   lines
@@ -18,58 +19,83 @@ fn parse_springs(input: &str, folds: usize) -> Vec<(Vec<char>, Vec<i64>)> {
       line.split_once(' ').map(|(springs, counts)| {
         (
           intersperse(repeat(springs).take(folds), "?").flat_map(str::chars).collect::<Vec<_>>(),
-          repeat(counts).take(folds).flat_map(|x| x.split(',').map(parse::<i64>)).collect::<Vec<_>>(),
+          repeat(counts).take(folds).flat_map(|x| x.split(',').map(parse::<u8>)).collect::<Vec<_>>(),
         )
       })
     })
     .collect::<Vec<_>>()
 }
 
-fn arrangements<'a>(cache: &mut Cache<'a>, row: &'a [char], matches: &'a [i64]) -> i64 {
-  cache.get(&(row, matches)).copied().unwrap_or_else(|| {
-    if row.is_empty() {
-      return match matches {
-        [] => 1,
-        _ => 0,
-      };
-    }
+fn cache_key(row: &[char], matches: &[u8]) -> usize {
+  (row.len() << 5) | matches.len()
+}
 
-    if matches.is_empty() {
-      return match row.contains(&'#') {
-        true => 0,
-        false => 1,
-      };
-    }
+fn arrangements<'a>(cache: &mut Cache<'a>, row: &'a [char], matches: &'a [u8]) -> i64 {
+  match cache[cache_key(row, matches)] {
+    i64::MAX => {}
+    memoized => return memoized,
+  }
 
-    let mut result = 0;
+  if let ['.', rest @ ..] = row {
+    return arrangements(cache, rest, matches);
+  }
 
-    let needle = matches[0] as usize;
-    let symbol = row[0];
+  if row.is_empty() {
+    return match matches {
+      [] => 1,
+      _ => 0,
+    };
+  }
 
-    if ['.', '?'].contains(&symbol) {
-      result += arrangements(cache, &row[1..], matches);
-    }
+  if matches.is_empty() {
+    return match row.contains(&'#') {
+      true => 0,
+      false => 1,
+    };
+  }
 
-    if ['#', '?'].contains(&symbol) && needle <= row.len() && !row[..needle].contains(&'.') && (needle == row.len() || row[needle] != '#') {
-      result += arrangements(cache, row.get(needle + 1..).unwrap_or(&[]), &matches[1..])
-    }
+  let mut result = 0;
 
-    cache.insert((row, matches), result);
+  let needle = matches[0] as usize;
+  let symbol = row[0];
 
-    result
-  })
+  if symbol == '?' {
+    result += arrangements(cache, &row[1..], matches);
+  }
+
+  if ['#', '?'].contains(&symbol) && needle <= row.len() && !row[..needle].contains(&'.') && (needle == row.len() || row[needle] != '#') {
+    result += arrangements(cache, row.get(needle + 1..).unwrap_or(&[]), &matches[1..])
+  }
+
+  cache[cache_key(row, matches)] = result;
+
+  result
 }
 
 pub fn part1(input: &str) -> i64 {
   let map = parse_springs(input, 1);
-  let mut cache: Cache = FxHashMap::default();
+  let mut cache: Cache = [i64::MAX; CACHE_SIZE];
 
-  map.iter().map(|(row, broken)| arrangements(&mut cache, row, broken)).sum()
+  map
+    .iter()
+    .map(|(row, broken)| {
+      cache.fill(i64::MAX);
+
+      arrangements(&mut cache, row, broken)
+    })
+    .sum()
 }
 
 pub fn part2(input: &str) -> i64 {
   let map = parse_springs(input, 5);
-  let mut cache: Cache = FxHashMap::default();
+  let mut cache: Cache = [i64::MAX; CACHE_SIZE];
 
-  map.iter().map(|(row, broken)| arrangements(&mut cache, row, broken)).sum()
+  map
+    .iter()
+    .map(|(row, broken)| {
+      cache.fill(i64::MAX);
+
+      arrangements(&mut cache, row, broken)
+    })
+    .sum()
 }
